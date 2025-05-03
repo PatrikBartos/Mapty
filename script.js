b31d5397ef46ff0136140a17a3d58825b8a3e427
Patrik Bartos <bartospatrik87@gmail.com>
2025-05-03 08:29:58 +0300

    Workout Input form gerendert
    - Ln 37 Input Field hinzugefuegt beim klicken auf der Karte.
    
    -  Ln 39 focus hinzugefuegt.
    
    - EventHandler hinzugefuegt beim klicken auf form .
    - Clear input fields hinzugefuegt, sodass beim Enter die Felder geloescht werden.
    
    - EventHandler zum wechseln von inputElevation und inputCadence.

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent;
+
 // Die erste Funktion ist die Funktion die ausgefuert wird wenn die Website den Zugriff auf den Standort bekommt, die zweite wenns nicht klappt
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
@@ -22,34 +24,57 @@ if (navigator.geolocation)
       //   const coords = Standort vom Benutzer, wo die Karte hingerichtet werden soll
       const coords = [latitude, longitude];
 
-      const map = L.map('map').setView(coords, 13);
+      map = L.map('map').setView(coords, 13);
 
       L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      map.on('click', function (mapEvent) {
-        console.log(mapEvent);
-        const { lat, lng } = mapEvent.latlng;
+      // Handling clicks on Map
+      map.on('click', function (mapE) {
+        mapEvent = mapE;
+        form.classList.remove('hidden');
 
-        L.marker([lat, lng])
-          .addTo(map)
-          .bindPopup(
-            L.popup({
-              maxWidth: 250,
-              minWidth: 100,
-              autoClose: false,
-              closeOnClick: false,
-              //   can add a css class
-              className: 'running-popup',
-            })
-          )
-          .setPopupContent('Workout')
-          .openPopup();
+        inputDistance.focus();
       });
     },
     function () {
       alert('Could not get your position');
     }
   );
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  // Clear input fields
+
+  inputDistance.value =
+    inputDuration.value =
+    inputCadence.value =
+    inputElevation.value =
+      '';
+
+  // Display marker
+  console.log(mapEvent);
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        //   can add a css class
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('Workout')
+    .openPopup();
+});
+
+inputType.addEventListener('change', function () {
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
